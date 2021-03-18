d49c23f71deb305515c3891cc2f88dac24a6e33f
Ousama Dahbi Sebbaghi <ods883@inlumine.ual.es>
2021-03-18 13:33:48 +0100

    Arreglos

--- Algebra_Allens/__init__.py
+++ Algebra_Allens/__init__.py
@@ -5,14 +5,15 @@ import sys
 
 from pbr.version import VersionInfo
 
-_v = VersionInfo('Algebra_Allen').semantic_version()
+_v = VersionInfo('Algebra_Allens').semantic_version()
 __version__ = _v.release_string()
 version_info = _v.version_tuple()
 
 __author__ = "Ousama Dahbi Sebbaghi"
 __copyright__ = "Copyright 2021-, Ousama Dahbi Sebbaghi"
+__email__ = "ods883@inlumine.ual.es"
 __status__ = "Alpha"
-__all__ = ('Algebra_Allen')
+__all__ = ('Algebra_Allens, __version__')
 
 if sys.hexversion < 0x02050000:
     sys.exit("Python 2.5 or newer is required by tendo module.")
